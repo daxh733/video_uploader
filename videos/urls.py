7d2996b96d97b9bde6a6a587f480e008e793eecf
Daksh Tyagi <tyagidaksh360@gmail.com>
2025-08-28 22:11:24 +0530

    with the form and enhance ui

--- videos/urls.py
+++ videos/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
-    path("upload/", views.upload_video, name="upload_video"),
+
+urlpatterns = [
+    path("upload/", views.upload_video, name="upload_video"),  # API endpoint
+    path("upload-web/", views.upload_video_web, name="upload_video_web"),  # Web form
     path("list/", views.list_videos, name="list_videos"),
+    path('show/', views.video_list, name='video_list'),
+    path('stream/<str:file_id>/', views.serve_video, name='serve_video'),
 ]
+
+    
+
+
--- videos/views.py
+++ videos/views.py
@@ -1,8 +1,13 @@
+from urllib import response
+import django
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import gridfs
 from bson import ObjectId
 from .mongo_client import db
+from .models import Video 
+from django.shortcuts import render,redirect
+from django.http import StreamingHttpResponse, Http404
 
 fs = gridfs.GridFS(db)
 
@@ -39,3 +44,48 @@ def list_videos(request):
         if "file_id" in v:
             v["file_id"] = str(v["file_id"])
     return JsonResponse(videos, safe=False)
+
+
+def video_list(request):
+    videos = list(db.videos.find({}))
+    filtered_videos = []
+    for v in videos:
+        v["_id"] = str(v["_id"])
+        if "file_id" in v and v["file_id"]:
+            v["file_id"] = str(v["file_id"])
+            filtered_videos.append(v)
+    return render(request, 'videos/video_list.html', {'videos': filtered_videos})
+
+
+def serve_video(request, file_id):
+    try:
+        file_obj=fs.get(ObjectId(file_id))
+        response=StreamingHttpResponse(file_obj,content_type="video/mp4")
+        response['Content-Disposition']=f'inline;filename="{file_obj.filename}"'
+        return response
+    except Exception as e:
+        raise Http404("Video not found")
+    
+def upload_video_web(request):
+    if request.method=='POST':
+        video_file=request.FILES.get("video")
+        title=request.POST.get("title")
+
+        if not title:
+           return render(request, 'videos/upload_video.html', {'error': "No title provided"})
+        if not video_file.content_type.startswith('video/'):
+            return render(request, 'videos/upload_video.html', {'error': "Only video files are allowed."})
+
+       # store file in GridFS
+        file_id = fs.put(video_file, filename=video_file.name)
+
+       # save meta in videos collection
+        videos = db["videos"]
+        videos.insert_one({
+           "title": title,
+           "file_id": file_id
+       })
+
+        return redirect('video_list')
+
+    return render(request, 'videos/upload_video.html')
